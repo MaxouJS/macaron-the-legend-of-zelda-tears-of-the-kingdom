d7c8897122d7fa192d649bf83dded41682b74714
MaxenceGumiero <117424624+MaxouJS@users.noreply.github.com>
2023-03-21 13:23:53 +1100

    Updating UI

--- src/components/static/navigation/Navigation.jsx
+++ src/components/static/navigation/Navigation.jsx
@@ -2,17 +2,14 @@ import Logo from "../../../../public/images/Logo.svg";
 
 export default function Navigation() {
   return (
-    <div className="w-full bg-gradient-to-r from-[red] via-transparent to-[red] bg-clip-padding backdrop-filter backdrop-blur-md shadow-xl shadow-white/10 border-b border-white/50 l text-white z-10 font-sans">
+    <div className="fixed w-full bg-gradient-to-r from-[red] via-transparent to-[red] backdrop-blur-md shadow-xl shadow-white/10 border-b border-white/50 l text-white z-10 font-sans">
       <nav className="flex items-center">
         <div className="flex container mx-auto p-4">
           <a href="/" className="font-black md:flex hidden hover:scale-125 duration-200 ease-out">
             <img src={Logo} className="h-8" />
           </a>
-          <div className="ml-auto space-x-8 md:flex hidden items-center text-xs font-medium">
-            <a href="#explore" className="hover:scale-125 duration-200 ease-out">Explore</a>
-            <a href="#pre-order" className="hover:scale-125 duration-200 ease-out">Pre-Order</a>
-          </div>
-          <div className="mx-auto space-x-8 md:hidden flex items-center text-xs font-medium">
+          <div className="flex md:ml-auto md:mx-0 mx-auto space-x-8 items-center text-xs font-medium">
+            <a href="#home" className="hover:scale-125 duration-200 ease-out">Home</a>
             <a href="#explore" className="hover:scale-125 duration-200 ease-out">Explore</a>
             <a href="#pre-order" className="hover:scale-125 duration-200 ease-out">Pre-Order</a>
           </div>
--- src/scenes/What.jsx
+++ src/scenes/What.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Vector3 } from "three";
 import { CameraShake, Loader, PerspectiveCamera, Sky } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { EffectComposer } from "@react-three/postprocessing";
+import { Bloom, DepthOfField, EffectComposer, HueSaturation, Noise, Vignette } from "@react-three/postprocessing";
 
 import Logo from "/images/Logo.png";
 
@@ -35,17 +35,26 @@ export default function What() {
   return (
     <section id="home" className="snap-start h-screen w-screen text-neutral-900">
       {/* 3D Canvas */}
-      <Canvas>
+      <Canvas
+        className="bg-gradient-to-b from-blue-800 via-teal-400 to-sky-50"
+      >
         {/* Postprocessing effects */}
-        <EffectComposer />
+        {/*
+        <EffectComposer>
+          <HueSaturation saturation={0.125} />
+          <Noise opacity={0.05} />
+          <DepthOfField focusDistance={0} focalLength={0.1} bokehScale={10} height={1024} />
+          <Bloom intensity={1} luminanceThreshold={1} luminanceSmoothing={1} height={1024} />
+          <Vignette offset={0.1} darkness={0.75} />
+        </EffectComposer>
+        */}
         {/* 2D UI */}
         {/* Camera and camera effects */}
         <Rig />
         <PerspectiveCamera>
           {/* Lights */}
-          <ambientLight />
-          <directionalLight position={[5, 20, 10]} />
-          <Sky />
+          <ambientLight intensity={0.75} />
+          <pointLight position={[5, 20, 10]} intensity={0.75} />
           {/* 3D Models */}
           <Model
             name="Box"
@@ -86,12 +95,12 @@ export default function What() {
         <Navigation />
         <div className="flex h-screen w-screen items-center justify-center text-center container mx-auto p-8 text-neutral-50">
           <header className="flex flex-col items-center justify-center space-y-8">
-            <div className="flex items-center bg-clip-padding backdrop-filter backdrop-blur-md hover:scale-110 duration-200">
-              <h1 className="lg:text-2xl md:text-xl text-lg font-black">Your journey starts </h1>
-            </div>
+            <h1 className="backdrop-blur-none pointer-events-none">
+              <img src={Logo} className="md:h-[50vh] h-[25vh] duration-200" />
+            </h1>
             <div className="flex md:space-x-8 space-x-4 duration-200 lg:text-2xl md:text-xl text-lg font-black ">
-              <a href="#explore" className="flex items-center bg-gradient-to-br from-teal-600/75 to-teal-800/75 bg-clip-padding backdrop-filter backdrop-blur-md shadow-xl shadow-white/10 lg:px-8 md:px-6 px-4 lg:py-4 md:py-3 py-2 hover:scale-110 duration-200">Explore</a>
-              <a href="#pre-order" className="flex items-center bg-gradient-to-br from-neutral-50/75 to-neutral-200/75 text-teal-800 bg-clip-padding backdrop-filter backdrop-blur-md shadow-xl shadow-white/10 lg:px-8 md:px-6 px-4 lg:py-4 md:py-3 py-2 hover:scale-110 duration-200">Pre-Order</a>
+              <a href="#explore" className="flex items-center bg-gradient-to-br from-teal-600/75 to-teal-800/75 backdrop-blur-md shadow-xl shadow-white/10 ring-2 ring-teal-600/75 ring-offset-neutral-50/100 ring-offset-2 lg:px-8 md:px-6 px-4 lg:py-4 md:py-3 py-2 hover:scale-110 hover:animate-pulse hover:ring-offset-teal-200/100 hover:text-teal-200 ease-out duration-200">Explore</a>
+              <a href="#pre-order" className="flex items-center bg-gradient-to-br from-neutral-50/75 to-neutral-200/75 text-teal-800 backdrop-blur-md shadow-xl shadow-white/10 ring-2 ring-neutral-50/75 ring-offset-teal-800/75 ring-offset-2 lg:px-8 md:px-6 px-4 lg:py-4 md:py-3 py-2 hover:scale-110 hover:animate-pulse hover:ring-offset-teal-400/100 hover:text-teal-400 ease-out duration-200">Pre-Order</a>
             </div>
           </header>
           <BottomArrow anchor="#explore" />
